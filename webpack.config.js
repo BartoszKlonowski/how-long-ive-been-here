55b751654d68fa30866810675bdb1b47050ba482
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-08-15 15:28:20 +0200

    Adjust compilation paths in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        "src/popup/Popup": "./build/src/popup/Popup.js",
-        "src/engine/Background": "./build/src/engine/Background.js",
+        "popup/Popup": "./build/src/popup/Popup.js",
+        "engine/Background": "./build/src/engine/Background.js",
     },
     output: {
         filename: "[name].js",
