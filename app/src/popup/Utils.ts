59efb012623ea742ef7b73d68d2b3b69e885016c
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-08-27 16:15:12 +0200

    Introduce Icon object interface

--- app/src/popup/Utils.ts
+++ app/src/popup/Utils.ts
@@ -1,16 +1,22 @@
+export interface Icon {
+    size: number;
+    src: string;
+}
+
 export const getActiveTabDomainFromDocument = (document: Document): string | null => {
     const results = document.URL.match(/[^https//.][^w.][^ /]+/g);
     return results && !results[0].includes(" ") ? results[0] : null;
 };
 
-export const getWebsiteIconObject = (websiteURL: string) => {
+export const getWebsiteIconObject = (websiteURL: string): Icon => {
     const iconDesiredSize = 16;
     const iconSource: string = websiteURL.length
         ? `http://www.google.com/s2/favicons?domain=${websiteURL}`
         : "../resources/missing-website-favicon.png";
 
-    return {
+    const icon: Icon = {
         size: iconDesiredSize,
         src: iconSource,
     };
+    return icon;
 };
