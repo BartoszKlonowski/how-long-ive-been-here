c2c42f95d9a6f75a8b79d93f8da1518729db8f7b
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-08-29 00:53:14 +0200

    Remove unused ShrinkedView tests

--- __tests__/subcomponents.test.tsx
+++ __tests__/subcomponents.test.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import TestRenderer, {ReactTestInstance, ReactTestRenderer} from "react-test-renderer";
+import TestRenderer, {ReactTestRenderer} from "react-test-renderer";
 import ShrinkedView from "../app/src/popup/subcomponents/ShrinkedView";
-import type {Browser} from "webextension-polyfill";
-import {deepMock, MockzillaDeep} from "mockzilla";
 
 function render(): ReactTestRenderer {
     const shrinkedView = TestRenderer.create(<ShrinkedView />);
     return shrinkedView;
-}
-
-function renderAsObject(): ReactTestInstance {
-    const shrinkedView = JSON.parse(JSON.stringify(TestRenderer.create(<ShrinkedView />).toJSON()));
-    return shrinkedView;
-}
-
-function getChild(renderedObject: ReactTestInstance, childIndex: number): ReactTestInstance {
-    const child = renderedObject.children[childIndex] as ReactTestInstance;
-    return child;
 }
 
 describe("ShrinkedView", () => {
