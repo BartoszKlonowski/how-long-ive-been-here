5935bb5886003ebafb8369def0b98c682d73e137
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-08-31 01:38:24 +0200

    Improve tests by introducing async act

--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -1,6 +1,7 @@
 if (!global.chrome) global.chrome = {};
 if (!global.chrome.runtime) global.chrome.runtime = {};
 if (!global.chrome.runtime.id) global.chrome.runtime.id = "testid";
+if (!global.browser) global.browser = {tabs: {}};
 
 test("Browser is defined correctly for web extension testing", () => {
     expect(global.chrome.runtime.id).toBe("testid");
--- __tests__/subcomponents.test.tsx
+++ __tests__/subcomponents.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TestRenderer, {ReactTestRenderer} from "react-test-renderer";
+import TestRenderer, {act, ReactTestInstance, ReactTestRenderer} from "react-test-renderer";
 import ShrinkedView from "../app/src/popup/subcomponents/ShrinkedView";
 
 function render(): ReactTestRenderer {
@@ -7,9 +7,35 @@ function render(): ReactTestRenderer {
     return shrinkedView;
 }
 
+async function renderAsObject(): Promise<ReactTestInstance> {
+    let component;
+    await act(async () => {
+        component = TestRenderer.create(<ShrinkedView />);
+    });
+    return JSON.parse(JSON.stringify(component.toJSON()));
+}
+
+function getChild(renderedObject: ReactTestInstance, childIndex: number): ReactTestInstance {
+    const child = renderedObject.children[childIndex] as ReactTestInstance;
+    return child;
+}
+
 describe("ShrinkedView", () => {
     it("renders correctly according to snapshot", () => {
         const shrinkedView = render();
         expect(shrinkedView.toJSON()).toMatchSnapshot();
+    });
+
+    it("renders icon as image with default src", async () => {
+        global.browser.tabs.query = () => {
+            return new Promise((resolve) => {
+                resolve([{url: "very-fake-website"}]);
+            });
+        };
+        const shrinkedView = await renderAsObject();
+        const img = getChild(shrinkedView, 0);
+        expect(img).toBeDefined();
+        expect(img.type).toBe("img");
+        expect(img.props.src).toContain("http://www.google.com/s2/favicons?domain=very-fake-website");
     });
 });
