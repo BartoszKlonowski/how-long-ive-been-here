9bdee71014bc9498172841a674e593a367b5b942
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-09-03 20:38:34 +0200

    Lint the webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,15 @@
-import * as path from 'path';
-import * as webpack from 'webpack';
+import * as path from "path";
+import * as webpack from "webpack";
 
 const config: webpack.Configuration = {
-  entry: {
-    "src/popup/Popup": "./build/src/popup/Popup.js",
-    "src/engine/Background": "./build/src/engine/Background.js",
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
-  },
+    entry: {
+        "src/popup/Popup": "./build/src/popup/Popup.js",
+        "src/engine/Background": "./build/src/engine/Background.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "build"),
+    },
 };
 
 export default config;
