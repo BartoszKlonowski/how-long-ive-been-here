d7a33d21b175b3240afd62032ca09fa06bdd8e6a
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-09-04 16:15:59 +0200

    Utilize webextension-polyfill in Background for types

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -1,9 +1,9 @@
-//@ts-ignore
+import browser from "webextension-polyfill";
+
 browser.tabs.onActivated.addListener((activeInfo) => {
     console.log("browser.tabs.onActivated with activeInfo: ", activeInfo);
 });
 
-//@ts-ignore
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     console.log("browser.tabs.onUpdated for tabId: ", tabId);
     if (changeInfo.status == "complete") {
