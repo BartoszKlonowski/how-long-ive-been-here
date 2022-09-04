b766a323c22514437f485148bcf6e559d7a6087b
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-09-04 16:15:30 +0200

    Use onUpdated and onActivated events for tabs

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -1,6 +1,12 @@
-// @ts-ignore
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    message;
-    sender;
-    sendResponse;
+//@ts-ignore
+browser.tabs.onActivated.addListener((activeInfo) => {
+    console.log("browser.tabs.onActivated with activeInfo: ", activeInfo);
+});
+
+//@ts-ignore
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log("browser.tabs.onUpdated for tabId: ", tabId);
+    if (changeInfo.status == "complete") {
+        console.log("browser.tabs.onUpdated with tab: ", tab);
+    }
 });
