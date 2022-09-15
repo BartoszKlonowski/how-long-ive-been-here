fc320ba56656489230cc51eb65d2939cc95e3a1f
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-09-15 21:56:03 +0200

    Engine - Background & Database (#7)
    * Use onUpdated and onActivated events for tabs
    
    * Utilize webextension-polyfill in Background for types
    
    * Read and Write all data regarding websites
    
    * Calculate time spent on last active URL
    
    * Format the storeTimeSpentSummary function
    
    * Summarize time spent only for different domain

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -1,6 +1,30 @@
-// @ts-ignore
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    message;
-    sender;
-    sendResponse;
+import browser from "webextension-polyfill";
+import {getActiveTabDomainFromURL} from "../popup/Utils";
+import Database from "./Database";
+
+function storeTimeSpentSummary(currentDomain: string) {
+    const db = new Database();
+    const previousDomain = db.readPreviousDomain();
+
+    if (currentDomain.length > 0 && previousDomain !== currentDomain) {
+        db.writePreviousDomain(currentDomain);
+        db.writeLastActive(currentDomain, new Date());
+
+        const lastActive = db.readLastActive(previousDomain);
+        const timeSpent = Math.trunc(Math.abs(Date.now() - lastActive.getTime()) / 1000);
+        const totalTimeSpentOfLastActive = (db.readTimeSpent(previousDomain) as number) + timeSpent;
+        db.writeTimeSpent(previousDomain, totalTimeSpentOfLastActive);
+    }
+}
+
+browser.tabs.onActivated.addListener((activeInfo) => {
+    browser.tabs.get(activeInfo.tabId).then((tab) => {
+        storeTimeSpentSummary(getActiveTabDomainFromURL(tab?.url || "") || "");
+    });
+});
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status == "complete") {
+        storeTimeSpentSummary(getActiveTabDomainFromURL(tab.url || "") || "");
+    }
 });
