676b764d0c26bbdf85890a2bd5792b21c192362d
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-09-15 21:10:47 +0200

    Summarize time spent only for different domain

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -3,15 +3,15 @@ import {getActiveTabDomainFromURL} from "../popup/Utils";
 import Database from "./Database";
 
 function storeTimeSpentSummary(currentDomain: string) {
-    if (currentDomain.length > 0) {
-        const db = new Database();
+    const db = new Database();
+    const previousDomain = db.readPreviousDomain();
 
-        const previousDomain = db.readPreviousDomain();
+    if (currentDomain.length > 0 && previousDomain !== currentDomain) {
         db.writePreviousDomain(currentDomain);
         db.writeLastActive(currentDomain, new Date());
 
         const lastActive = db.readLastActive(previousDomain);
-        const timeSpent = Math.abs(Date.now() - lastActive.getTime());
+        const timeSpent = Math.trunc(Math.abs(Date.now() - lastActive.getTime()) / 1000);
         const totalTimeSpentOfLastActive = (db.readTimeSpent(previousDomain) as number) + timeSpent;
         db.writeTimeSpent(previousDomain, totalTimeSpentOfLastActive);
     }
