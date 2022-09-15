4802fc1f5c562be025611a75d22ab32dd6217b05
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-09-15 20:35:47 +0200

    Calculate time spent on last active URL

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -1,12 +1,29 @@
 import browser from "webextension-polyfill";
+import { getActiveTabDomainFromURL } from "../popup/Utils";
+import Database from "./Database";
+
+function timeSpentSummary(currentDomain: string) {
+    if(currentDomain.length > 0) {
+        const db = new Database();
+        const lastDomain = db.readPreviousDomain();
+        db.writePreviousDomain(currentDomain);
+        const lastActive = db.readLastActive(lastDomain);
+        const timeSpent = Math.abs(Date.now() - lastActive.getTime());
+        let totalTimeSpentOfLastActive = db.readTimeSpent(lastDomain) as number;
+        totalTimeSpentOfLastActive += timeSpent;
+        db.writeLastActive(currentDomain, new Date());
+        db.writeTimeSpent(lastDomain, totalTimeSpentOfLastActive);
+    }
+}
 
 browser.tabs.onActivated.addListener((activeInfo) => {
-    console.log("browser.tabs.onActivated with activeInfo: ", activeInfo);
+    browser.tabs.get(activeInfo.tabId).then((tab) => {
+        timeSpentSummary(getActiveTabDomainFromURL(tab?.url || "") || "");
+    })
 });
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log("browser.tabs.onUpdated for tabId: ", tabId);
     if (changeInfo.status == "complete") {
-        console.log("browser.tabs.onUpdated with tab: ", tab);
+        timeSpentSummary(getActiveTabDomainFromURL(tab.url || "") || "");
     }
 });
