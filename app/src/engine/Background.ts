3fffbd7f6e87510d49919b7d8e2e01eb3805f043
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-10-07 19:57:24 +0200

    Handle browser focus and blur regarding time summary (#24)
    * Add listener to focus change of browser window
    
    * Extract time calculation from time summarizing function
    
    * Handle the focus and blur by storing data
    
    * Remove unused focus event listener

--- app/src/engine/Background.ts
+++ app/src/engine/Background.ts
@@ -1,6 +1,7 @@
 import browser from "webextension-polyfill";
-import {getActiveTabDomainFromURL} from "../popup/Utils";
+import {calculateTimeSpentForDomain, getActiveTabDomainFromURL} from "../popup/Utils";
 import {storeTimeSpentSummary} from "../popup/Utils";
+import Database from "./Database";
 
 browser.tabs.onActivated.addListener((activeInfo) => {
     browser.tabs.get(activeInfo.tabId).then((tab) => {
@@ -11,5 +12,20 @@ browser.tabs.onActivated.addListener((activeInfo) => {
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == "complete") {
         storeTimeSpentSummary(getActiveTabDomainFromURL(tab.url || "") || "");
+    }
+});
+
+browser.windows.onFocusChanged.addListener((id) => {
+    const windowInactiveID = -1;
+    const db = new Database();
+    if (id === windowInactiveID) {
+        calculateTimeSpentForDomain(db.readPreviousDomain());
+        db.writePreviousDomain("");
+    } else {
+        browser.tabs.query({active: true}).then((tab) => {
+            const newFocusedDomain = getActiveTabDomainFromURL(tab[0].url || "");
+            db.writeLastActive(newFocusedDomain || "", new Date());
+            db.writePreviousDomain(newFocusedDomain || "");
+        });
     }
 });
